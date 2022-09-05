4cec0ac4e9ea589904f55b5a24ed46b7c92715c4
Cainã Perotti Barbosa <caiperotti@gmail.com>
2022-09-05 19:58:01 -0300

    Consertando o bug pra renderizar sem gambiarra

--- pokedex/src/pages/Home/Home.js
+++ pokedex/src/pages/Home/Home.js
@@ -9,30 +9,25 @@ import { Button, Footer, Header, HomePage, Main } from './HomeStyle';
 
 export function Home (){
     const navigate = useNavigate();
-    const [pokeList, setpokeList] = useState([])
-    const [pokeFront, setpokeFront] = useState(undefined)
-    
+    const [pokeFront, setpokeFront] = useState([])
     const BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
     const limit = 20;
     const page = 0;
     const offset = page * 0;
 
-    function callbackfn (pokemon) {
-        return {name: pokemon.name, url: pokemon.url}
-    }
 
     useEffect(() => {
         axios.get(`${BASE_URL}`).then((response) => {
-            setpokeList(response.data.results.map(callbackfn).map((pokemon, index) => {
-                return {name: pokemon.name, img: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${index+1}.png`}
-            }))
-            console.log(pokeList)            
+            const pokeArray = []
+            response.data.results.map((pokemon) => {return {name: pokemon.name, url: pokemon.url}}).map((pokemon, index) => {
+                axios.get(pokemon.url).then((response) => {
+                    pokeArray.push({name: response.data.name, img: response.data.sprites.front_default, key: (index)})
+                    setpokeFront([...pokeArray])
+                })
+            })                
         })
-        
     }
-    ,[])
-
-    const pokeDisplay = pokeList.map((pokemon) => {return <PokemonCards pokeName = {pokemon.name} pokeImage = {pokemon.img} ></PokemonCards>})
+    ,[page])
 
 
     return (
@@ -42,7 +37,7 @@ export function Home (){
             <Button onClick={() => RoutePages.goPokedex(navigate)}>Ir para pokedex</Button>
         </Header>
         <Main>
-            {pokeDisplay}
+            {pokeFront.map((pokemon) => {return <PokemonCards pokeName = {pokemon.name} pokeImage = {pokemon.img} key= {pokemon.key} ></PokemonCards>})}
         </Main>
         <Footer>
         </Footer>
