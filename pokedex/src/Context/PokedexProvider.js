f87b026ad52df99fac8b8860d4d7e260e211e5ae
Caroline Ávila <102264452+avilacarol-bit@users.noreply.github.com>
2022-09-08 21:41:00 -0300

    implementando a lógica de add na pokedex

--- pokedex/src/Context/PokedexProvider.js
+++ pokedex/src/Context/PokedexProvider.js
@@ -7,20 +7,18 @@ export const PokedexProvider = (props) => {
     
     const addToPokedex = (pokemon) => {
         const newPokemon = [...addPokemon]
+        // console.log(pokemon)
 
-        const pokemonIndex = addPokemon.findIndex((p) => p.id === pokemon.id)
+        const pokemonIndex = newPokemon.findIndex((p) => p === pokemon)
 
         if (pokemonIndex === -1) {
-            
-            newPokemon.push({...pokemon, quantity: 1})
+            newPokemon.push(pokemon)
             console.log(`pokemon adicionado.`)
-
+            setAddPokemon(newPokemon)
+            
         } else {
-            
-            
             console.log(`pokemon já foi adicionado à pokedex.`)
         }
-        setAddPokemon(newPokemon)
 
         }
 
@@ -29,11 +27,11 @@ export const PokedexProvider = (props) => {
     const removePokemon = (pokemon) => {
         const newPokemon = [...addPokemon]
 
-        const pokemonIndex = addPokemon.findIndex((p) => p.id === pokemon.id)
+        const pokemonIndex = addPokemon.findIndex((p) => p === pokemon)
 
         if (newPokemon[pokemonIndex].quantity === 1) {
             newPokemon.splice(pokemonIndex, 1)
-            
+            setAddPokemon(newPokemon)
         } else {
             addToPokedex()
         }
